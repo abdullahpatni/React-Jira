8cf05b20f1ad8f593e3bab7b3d851841db7f6ee2
Abdullah Patni <abdullahpanja@gmail.com>
2022-10-19 19:38:31 +0530

    Lanes and dataset

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import dataset from "./dataset";
 import Column from "./Column";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const Container = styled.div`
 	display: flex;
 	flex-wrap: wrap;
+	justify-content: space-between
 `;
+
+const dataset = {
+	tasks: {
+		"task-1": { id: "task-1", content: "Content for task 1" },
+		"task-2": { id: "task-2", content: "Content for task-2" },
+		"task-3": { id: "task-3", content: "Content for task-3" },
+		"task-4": { id: "task-4", content: "Content for task-4" },
+	},
+	columns: {
+		"column-1": { id: "column-1", title: "Todo", taskIds: ["task-1"] },
+		"column-2": { id: "column-2", title: "In progress", taskIds: ["task-2", "task-3"] },
+		"column-3": { id: "column-3", title: "Review", taskIds: [] },
+		"column-4": { id: "column-4", title: "Completed", taskIds: ["task-4"] },
+	},
+	columnOrder: ["column-1", "column-2", "column-3", "column-4"],
+};
 
 const App = () => {
 	const [data, setData] = useState(dataset);
+	const [task, setTask] = useState("");
+	const handleChange = (event) => {
+		setTask(event.target.value);
+		// console.log(task)
+	};
+	const onAddTask = () => {
+		console.log(task);
+		// setData(current => cu)
+		// setData(current => current.tasks[`task-${task}`] = { id: `task-${task}`, content: task })
+		// setData(current => data.columns["column-1"] = { ...current.columns["column-1"], taskIds: [...current.columns["column-1"].taskIds, task] })
+		setData(current => console.log(current))
+		const toPush =  { id: `task-${task}`, content: task }
+		setData(current => ({
+			...current,
+			[current.tasks[`task-${task}`] : 'h
+			
+			'] 
+		}))
+		// setData(current => )
+		// localStorage.setItem('myData', data)
+		// console.log(data)
+	};
 
 	const onDragEnd = (result) => {
 		const { destination, source, draggableId, type } = result;
@@ -73,20 +113,26 @@ const App = () => {
 	};
 
 	return (
-		<DragDropContext onDragEnd={onDragEnd}>
-			<Droppable droppableId="all-columns" direction="horizontal" type="column">
-				{(provided) => (
-					<Container {...provided.droppableProps} ref={provided.innerRef}>
-						{data.columnOrder.map((id, index) => {
-							const column = data.columns[id];
-							const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
-							return <Column key={column.id} column={column} tasks={tasks} index={index} />;
-						})}
-						{provided.placeholder}
-					</Container>
-				)}
-			</Droppable>
-		</DragDropContext>
+		<>
+			<Container>
+				<TextField id="outlined-basic" label="Task"  variant="outlined" value={task} onChange={handleChange} />
+				<Button variant="contained" onClick={() => { onAddTask(); }}>Add Task</Button>
+			</Container>
+			<DragDropContext onDragEnd={onDragEnd}>
+				<Droppable droppableId="all-columns" direction="horizontal" type="column">
+					{(provided) => (
+						<Container {...provided.droppableProps} ref={provided.innerRef}>
+							{data.columnOrder.map((id, index) => {
+								const column = data.columns[id];
+								const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
+								return <Column key={column.id} column={column} tasks={tasks} index={index} />;
+							})}
+							{provided.placeholder}
+						</Container>
+					)}
+				</Droppable>
+			</DragDropContext>
+		</>
 	);
 };
 
